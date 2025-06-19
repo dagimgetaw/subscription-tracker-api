b144afb8876cb0e3bee8fe57c2db870c0b479b23
Dagim <dagimgetaW27@gmail.com>
2025-06-19 16:02:08 +0300

    add SERVER_URL and QSTASH in env

--- config/env.js
+++ config/env.js
@@ -4,10 +4,13 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 export const {
   PORT,
+  SERVER_URL,
   ENV,
   DB_URL,
   JWT_SECRET,
   JWT_EXPIRES_IN,
   ARCJET_KEY,
   ARCJET_ENV,
+  QSTASH_URL,
+  QSTASH_TOKEN,
 } = process.env;
